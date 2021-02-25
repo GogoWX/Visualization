3ebdc89a53ac1cfb0e1ea36dd3f465f9c65a2942
烟花易冷 <oncwnuE0HwT7n6V6X0co_IGM6dxk@git.weixin.qq.com>
2021-02-25 10:34:41 +0800

    updata

--- js/detail.js
+++ js/detail.js
@@ -22,3 +22,43 @@
         setFont();
     }
 })();
+
+//企业列表及切换
+(function () {
+    let clist = ['润联行·时代', '润联行·西堤', '凤凰置地广场']
+    clist.map((company, index) => {
+        let chtml = `<option value="${index}">
+                        <h3>${company}</h3>
+                    </option>`
+        $('#company').append(chtml)
+    })
+})();
+
+
+function changeD() {
+    let chooseD = $('#company').val();
+    console.log(chooseD)
+}
+
+(function () {
+    //事件委托
+    // $('.monitor').on('click', ' a', function () {
+    //     //点击当前的a 加类名 active  他的兄弟删除类名
+    //     $(this).addClass('active').siblings().removeClass('active');
+    //     //获取一一对应的下标 
+    //     var index = $(this).index();
+    //     //选取content 然后狗日对应下标的 显示   当前的兄弟.content隐藏
+    //     $('.content').eq(index).show().siblings('.content').hide();
+    // });
+    //滚动
+    //原理：把marquee下面的子盒子都复制一遍 加入到marquee中
+    //      然后动画向上滚动，滚动到一半重新开始滚动
+    //因为选取的是两个marquee  所以要遍历
+    $('.monitor .marquee').each(function (index, dom) {
+        //将每个 的所有子级都复制一遍
+        var rows = $(dom).children().clone();
+        //再将新的到的加入原来的
+        $(dom).append(rows);
+    });
+
+})();
